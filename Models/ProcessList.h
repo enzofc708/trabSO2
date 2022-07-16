0c966dc608a5a181e62f80affd142744d993c2c1
enzo <enzocarnevali708@gmail.com>
2022-07-16 12:23:33 -0300

    pela metade

--- Models/ProcessList.h
+++ Models/ProcessList.h
@@ -1,8 +1,9 @@
+// A basic implementation of a List, specified for the class Process
 typedef struct{
     Process* processList[MAX_PROCESSES];
     int count;
 } ProcessList;
-    
+
 //Instantiates a ProcessList and returns a pointer to it
 ProcessList* createProcessList(){
     ProcessList* list = (ProcessList *) malloc(sizeof(ProcessList));
@@ -13,4 +14,16 @@ ProcessList* createProcessList(){
 //Adds a Process to the list and updates the process counter
 void add(ProcessList* list, Process* p){
     list->processList[list->count++] = p;
+}
+
+Process* getOldestRunningProcess(ProcessList* list){
+    int oldest = -1;
+    Process* oldestP;
+    for (int i = 0; i < list->count; i++)
+    {
+        if(list->processList[i]->currentState == RunningState &&
+           list->processList[i]->statusTime < oldest &&
+           oldest )
+    }
+    
 }
